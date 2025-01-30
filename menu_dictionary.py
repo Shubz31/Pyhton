8fbbb179c798154d59a44bb483efb39f26458902
Shubz31 <shubham.kadam1971@gmail.com>
2025-01-30 19:21:01 +0530

    push main

---
+++ menu_dictionary.py
@@ -0,0 +1,61 @@
+def display_menu():
+    print("1.Display the Dictionary")
+    print("2. Print a specific Element")
+    print("3. OverWritten Dictionary")
+    print("4. Print Keys")
+    print("5. Print Values")
+    print("6. Print Items")
+    print("7. Pop(delete) Element")
+    print("8. Pop Item")
+    print("9. Clear Dictionary")
+    print("10. Exit")
+    
+def main():
+    dictName = {1: 'Java', 2: 'Web Development', 3: 'Pyhton'}
+    while(True):
+        display_menu()
+        choice = int(input("Enter your Choice: "))
+        
+        if choice == 1:
+            print(dictName)
+            
+        elif choice == 2:
+            element = int(input("Enter element you want to display: "))
+            print(dictName[element])
+            
+        elif choice == 3:
+            dictName = {'First' : 'Shubham', 'Second' : 'Sushant', 'Third' : 'Aditya'}
+            print(dictName)
+            
+        elif choice == 4:
+            print(dictName.keys())
+                
+        elif choice == 5:
+            print(dictName.values())
+            
+        elif choice == 6:
+            print(dictName.items())
+        
+        elif choice == 7:
+            poped = dictName['Third']
+            print('Poped_Element: ', poped)
+            print('Updated List: ', dictName)
+            
+        elif choice == 8:
+            poped_item = dictName.popitem()
+            print("Key, Values: ", poped_item)
+            print("Dictionary: ", dictName)
+            
+        elif choice == 9:
+            dictName.clear()
+            print(dictName)
+        
+        elif choice == 10:
+            exit()
+            break
+            
+        else:
+            print("Enter valid input")
+        
+if __name__ == "__main__":
+    main()       
